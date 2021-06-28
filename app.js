3c4cc862f3f9ff1ff05e1eb2d5dab7c39eb1e446
Mukhammad Razzakberganov <72851181+UzWebDeveloper@users.noreply.github.com>
2021-06-28 14:17:29 +0500

    web-news

---
+++ app.js
@@ -0,0 +1,8 @@
+const searchBox = document.querySelector(".search-box");
+const btn = document.querySelector('#btn')
+
+btn.addEventListener('')
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
